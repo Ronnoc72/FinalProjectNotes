badf9feee653e4ad331b139876f619f5c105c7c8
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2022-03-09 09:24:07 -0700

    Functionality of App

--- app/src/main/java/com/example/finalprojectnotes/NotesActivity.kt
+++ app/src/main/java/com/example/finalprojectnotes/NotesActivity.kt
@@ -1,16 +1,20 @@
 package com.example.finalprojectnotes
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import androidx.core.view.children
-import androidx.core.view.setPadding
+import android.widget.Toast
+import android.app.AlertDialog
+import android.widget.LinearLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import com.example.finalprojectnotes.NotesActivity as ComExampleFinalprojectnotesNotesActivity
+import java.util.*
+import kotlin.collections.ArrayList
 
 class NotesActivity : AppCompatActivity() {
 
@@ -20,14 +24,14 @@ class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
-
-        var gridLayout: androidx.gridlayout.widget.GridLayout = findViewById(R.id.gridLayout)
-        mAuth.currentUser?.uid?.let {
+        var linearLayout: LinearLayout = findViewById(R.id.linearLayout)
+        mAuth.currentUser?.uid?.let { test ->
             FirebaseDatabase.getInstance().reference.child("users")
-                .child(it).child("notes").addValueEventListener(object : ValueEventListener {
+                .child(test).child("notes").addValueEventListener(object : ValueEventListener {
+                    @SuppressLint("ResourceType")
                     override fun onDataChange(snapshot: DataSnapshot) {
                         notes.clear()
-                        gridLayout.removeAllViews()
+                        linearLayout.removeAllViews()
                         if (snapshot.exists()) {
                             for (s in snapshot.children) {
                                 var noteList: ArrayList<String> = ArrayList()
@@ -40,7 +44,33 @@ class NotesActivity : AppCompatActivity() {
                         }
                         for (note in notes) {
                             var textView = TextView(this@NotesActivity)
-                            textView.text = note[1]
+                            textView.setOnLongClickListener { task ->
+                                val builder = AlertDialog.Builder(this@NotesActivity)
+                                builder.setMessage(R.string.dialog_message)
+                                builder.setTitle("Delete Note?")
+                                builder.setCancelable(false)
+                                builder.setPositiveButton(R.string.okay, DialogInterface.OnClickListener { dialogInterface, i ->
+                                    mAuth.currentUser?.uid?.let {
+                                        FirebaseDatabase.getInstance().reference
+                                            .child("users")
+                                            .child(it).child("notes")
+                                            .child(note[2]).removeValue()
+                                    }
+                                })
+                                builder.setNegativeButton(R.string.cancel, DialogInterface.OnClickListener { dialogInterface, i ->
+                                    return@OnClickListener
+                                })
+                                val alertDialog = builder.create()
+                                alertDialog.show()
+                                true
+                            }
+                            textView.text = formatText(note[1].replaceFirstChar {
+                                if (it.isLowerCase()) it.titlecase(
+                                    Locale.getDefault()
+                                ) else it.toString()
+                            })
+                            textView.textSize = 24F
+                            textView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
                             textView.setOnClickListener { task ->
                                 var intent = Intent(this@NotesActivity, ViewNotesActivity::class.java)
                                 intent.putExtra("title", note[1])
@@ -50,7 +80,7 @@ class NotesActivity : AppCompatActivity() {
                             }
                             textView.height = 360
                             textView.width = 360
-                            gridLayout.addView(textView)
+                            linearLayout.addView(textView)
                         }
                     }
 
@@ -65,5 +95,17 @@ class NotesActivity : AppCompatActivity() {
     fun addNote(view: View) {
         val intent = Intent(this, CreateNoteActivity::class.java)
         startActivity(intent)
+    }
+
+    fun signOut(view: View) {
+        mAuth.signOut()
+        finish()
+    }
+
+    fun formatText(text: String): String {
+        if (text.length > 15) {
+            return text.substring(0, 15) + "..."
+        }
+        return text
     }
 }
